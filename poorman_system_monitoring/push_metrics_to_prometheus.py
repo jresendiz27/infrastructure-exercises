8abdadfd3cb816bda6250252bc750c218f098cfd
Juan Reséndiz <jresendiz27@gmail.com>
2020-05-01 00:07:32 -0500

    Add UID and openmetrics format

--- poorman_system_monitoring/push_metrics_to_prometheus.py
+++ poorman_system_monitoring/push_metrics_to_prometheus.py
@@ -1,10 +1,10 @@
 import asyncio
 import requests
-import json
+import aiohttp
 import csv
 
 MEMORY_COMMAND = 'head -n 1 /proc/meminfo | awk \'{print $2}\''
-PROCESS_INFORMATION = 'ps -o pid,user,%mem,command ax'
+PROCESS_INFORMATION = 'ps -o pid,uid,%mem,command ax'
 # PROCESS_INFORMATION = 'ps -eo pid,user,vsize,cmd'
 SSH_COMMAND_TO_RETRIEVE = "ssh " \
                           "-o UserKnownHostsFile=/dev/null " \
@@ -42,23 +42,30 @@ async def processes_information(params, current_server_memory):
         lines = ps_process_stdout.decode().splitlines()
         for i in range(1, len(lines)):
             columns = lines[i].split()
-            result = {
-                'process_id': int(columns[0]),
-                'process_owner': columns[1],
-                'process_memory_percentage': float(columns[2]),
-                'process_memory_usage': current_server_memory * float(columns[2]) / 1000,
-                'command': " ".join(columns[3:])
-            }
+            # command {" ".join(columns[3:])}
+            result = f'''
+process_id {int(columns[0])}
+# TYPE process_owner untyped
+process_owner '{columns[1]}'
+process_memory_percentage {float(columns[2])}
+process_memory_usage {current_server_memory * float(columns[2]) / 100}
+'''
+
             process_list.append(result)
-        print(json.dumps(process_list))
     return process_list
 
 
-async def push_to_prometheus(content):
+async def push_to_prometheus(host_info, list_processes):
     # aqui la wea del push
-    # requests.post("localhost:8000/metrics", json.dumps(content))
-    print("Here it goes the request using the prometheus client ...")
-    return True
+    host = f"http://localhost:9091/metrics/job/{host_info}"
+    results = []
+    for process in list_processes:
+        print(process)
+        async with aiohttp.ClientSession(headers={'Content-Type': 'text/plain'}).post(host, data=process) as resp:
+            print(f"{await resp.text()}")
+            results.append(f"{resp.text()} - {resp.status}")
+
+    return results
 
 
 async def main():
@@ -67,19 +74,22 @@ async def main():
         csv_reader = csv.DictReader(csv_file)
         for row in csv_reader:
             server_list.append(row)
-    print(f"Server List: {len(server_list)}")
+
     memory_results = [asyncio.ensure_future(memory_retrieval(values)) for values in server_list]
     await asyncio.wait(memory_results)
-    print(f"Memory Results: {len(memory_results)}")
+
     processes_results = [asyncio.ensure_future(processes_information(server_list[i], memory_results[i].result())) for i
                          in
                          range(0, len(server_list))]
     await asyncio.wait(processes_results)
-    print(f"Processes Results: {len(processes_results)}")
-    prometheus_push_results = [asyncio.ensure_future(push_to_prometheus(process.result())) for process in
-                               processes_results]
-    await asyncio.wait(prometheus_push_results)
 
+    prometheus_push_results = [
+        asyncio.ensure_future(
+            push_to_prometheus(f"{server_list[i]['host']}-{i}", processes_results[i].result())
+        ) for i in range(0, len(processes_results))]
+
+    await asyncio.wait(prometheus_push_results)
+    print("Hola")
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
